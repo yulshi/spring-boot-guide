090292ff47121f0602c4feb32d3ceb8ccaeea459
Yulong Shi <shiyulongwudi@gmail.com>
2019-06-25 23:33:20 +0800

    consuming restful web services

---
+++ consuming-rest/src/main/java/com/snail/springboot/DemoApplication.java
@@ -0,0 +1,37 @@
+package com.snail.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random",
+                    Quote.class);
+            logger.info(quote.toString());
+        };
+
+    }
+
+}
